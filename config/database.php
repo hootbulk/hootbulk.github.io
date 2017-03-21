8716ef60241434180a55cbbd9c283c234d0a3983
Luis Ramos <ljralonso@gmail.com>
2017-03-20 21:42:37 -0400

    Database Update

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 class Database{
 
     // specify your own database credentials
-    private $host = "localhost";
+    private $host = "localhost:8888";
     private $db_name = "hootbulk";
     private $username = "root";
     private $password = "root";
