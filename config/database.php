da3854bc4b48922de65e5fd431dae74a1621c532
Luis Ramos <ljralonso@gmail.com>
2017-03-21 00:44:58 -0400

    Database Environment

--- config/database.php
+++ config/database.php
@@ -1,11 +1,20 @@
 <?php
+
+$my_env_var = getenv('ENV_VAR');
+
 class Database{
 
-    // specify your own database credentials
-    private $host = "us-cdbr-iron-east-03.cleardb.net";
-    private $db_name = "heroku_14623f4742d3625";
-    private $username = "b374ad5080867e";
-    private $password = "5dca8c25";
+    if($my_env_var != 'production') {
+      private $host = "us-cdbr-iron-east-03.cleardb.net";
+      private $db_name = "heroku_14623f4742d3625";
+      private $username = "b374ad5080867e";
+      private $password = "5dca8c25";
+    } else {
+      private $host = "us-cdbr-iron-east-03.cleardb.net";
+      private $db_name = "heroku_f21a9b33cf39ec3";
+      private $username = "b34b087174976e";
+      private $password = "f2315a94";
+    }
     public $conn;
 
     // get the database connection
