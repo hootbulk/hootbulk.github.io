83d461ddd55bcb459552f90540638ff9e8d24baf
Luis Ramos <ljralonso@gmail.com>
2017-03-21 02:04:42 -0400

    Database Test

--- config/database.php
+++ config/database.php
@@ -1,39 +1,54 @@
 <?php
 
-  var $host = "us-cdbr-iron-east-03.cleardb.net";
-  var $db_name = "heroku_14623f4742d3625";
-  var $username = "b374ad5080867e";
-  var $password = "5dca8c25";
+$my_env_var = getenv('ENV_VAR');
 
-class Database{
+if($my_env_var != 'production') {
 
-    // $my_env_var = getenv('ENV_VAR');
-    //
-    // if($my_env_var != 'production') {
-    //   private $host = "us-cdbr-iron-east-03.cleardb.net";
-    //   private $db_name = "heroku_14623f4742d3625";
-    //   private $username = "b374ad5080867e";
-    //   private $password = "5dca8c25";
-    // } else {
-    //   private $host = "us-cdbr-iron-east-03.cleardb.net";
-    //   private $db_name = "heroku_f21a9b33cf39ec3";
-    //   private $username = "b34b087174976e";
-    //   private $password = "f2315a94";
-    // }
-    public $conn;
+  class Database{
 
-    // get the database connection
-    public function getConnection(){
+      private $host = "us-cdbr-iron-east-03.cleardb.net";
+      private $db_name = "heroku_14623f4742d3625";
+      private $username = "b374ad5080867e";
+      private $password = "5dca8c25";
+      public $conn;
 
-        $this->conn = null;
+      // get the database connection
+      public function getConnection(){
 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
-        }
+          $this->conn = null;
 
-        return $this->conn;
-    }
+          try{
+              $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+          }catch(PDOException $exception){
+              echo "Connection error: " . $exception->getMessage();
+          }
+
+          return $this->conn;
+      }
+  }
+
+} else {
+  
+  class Database{
+
+      private $host = "us-cdbr-iron-east-03.cleardb.net";
+      private $db_name = "heroku_f21a9b33cf39ec3";
+      private $username = "b34b087174976e";
+      private $password = "f2315a94";
+
+      // get the database connection
+      public function getConnection(){
+
+          $this->conn = null;
+
+          try{
+              $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+          }catch(PDOException $exception){
+              echo "Connection error: " . $exception->getMessage();
+          }
+
+          return $this->conn;
+      }
+  }
 }
 ?>
