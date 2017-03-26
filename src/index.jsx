1d91ffad81bf55f655e4cd92d32711d68ef2dfb1
Luis Ramos <ljralonso@gmail.com>
2017-03-25 21:48:48 -0400

    Changes

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BabelTransformer from './components/babel-transformer.jsx';
-
-ReactDOM.render(
-  <BabelTransformer />,
-  document.getElementById('app')
-)
