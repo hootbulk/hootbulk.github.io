d5755e5c32c750b64f50f65802d1e33009fda42d
Luis Ramos <ljralonso@gmail.com>
2017-03-29 16:33:39 -0400

    Three Columns

---
+++ src/Menu.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import 'whatwg-fetch';
+
+class Menu extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Pencil Menu</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Menu from './Menu';
 import './index.css';
 
 let baseURL = 'https://pokeapi.co/api/v2'
+
+ReactDOM.render(
+  <Menu />,
+  document.getElementById('menu')
+);
 
 ReactDOM.render(
   <App baseURL={baseURL} />,
